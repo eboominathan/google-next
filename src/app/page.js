6398140547edc07ddc93a58b98ed52c36191d5e7
Boominathan <eboominathan@gmail.com>
2024-03-07 07:00:44 +0530

    Body added

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,6 @@
 import HomeHeader from "@/components/HomeHeader";
+import HomeSearch from "@/components/HomeSearch";
+import Image from "next/image";
 
  
 
@@ -6,6 +8,17 @@ export default function Home() {
   return (   
     <>
       <HomeHeader/>
+      <div className="flex flex-col items-center mt-24">
+      <Image 
+      src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1280px-Google_2015_logo.svg.png"
+      width={300}
+      height={100}
+      alt="Google"
+      priority
+      style={{width:'auto'}}
+      />
+      <HomeSearch/>
+      </div>
     </>
   )
 }
